daf2d6b05c056bc4195fb7fea8600e166e3fc1cb
mhdtarek <mhdtarekalkasemi@gmail.com>
2023-07-08 12:59:23 +0200

    Privacy policy

---
+++ src/routes/privacy-policy/+page.svelte
@@ -0,0 +1,27 @@
+<h1>Privacy Policy for "Söderhamns Moské" Mobile Application</h1>
+  <h2>Effective Date: For two years from 2023-07-01</h2>
+
+  <p>At "Söderhamns Moské," we value your privacy and are committed to protecting any personal information you may provide while using our mobile application. This Privacy Policy outlines the types of information we collect, how we use it, and the measures we take to safeguard your data.</p>
+
+  <h3>1. Information We Do Not Collect</h3>
+  <p>We want to assure you that "Söderhamns Moské" does not collect or store any personal data, cookies, or any other form of personally identifiable information. We respect your privacy and have designed our application to operate without the need for such data.</p>
+
+  <h3>2. Anonymous Usage Data</h3>
+  <p>To enhance the performance and usability of our application, we may collect anonymous usage data. This information does not personally identify you and is used solely for analytical purposes. It helps us understand how our app is used, identify areas for improvement, and optimize the user experience.</p>
+
+  <h3>3. Device Information</h3>
+  <p>Our application may automatically collect certain information about your device, such as the type of mobile device you use, your device's unique identifier, operating system version, and other technical details. This data is collected to ensure compatibility, provide updates, and troubleshoot any issues that may arise.</p>
+
+  <h3>4. Third-Party Services</h3>
+  <p>"Söderhamns Moské" does not integrate any third-party services, such as analytics, advertising, or social media platforms, that may collect user data. We strive to maintain a secure and private environment for all users.</p>
+
+  <h3>5. Security Measures</h3>
+  <p>We employ industry-standard security measures to protect your data from unauthorized access, alteration, or disclosure. However, please note that no method of data transmission or storage is entirely secure, and we cannot guarantee absolute security.</p>
+
+  <h3>6. Changes to the Privacy Policy</h3>
+  <p>We may update this Privacy Policy from time to time. Any changes will be effective immediately upon posting the revised version in our application. We encourage you to review this policy periodically for any updates. Your continued use of the "Söderhamns Moské" mobile application constitutes your acceptance of any changes made.</p>
+
+  <h3>7. Contact Us</h3>
+  <p>If you have any questions or concerns regarding this Privacy Policy or our data practices, please contact us at [insert contact information].</p>
+
+  <p>By using the "Söderhamns Moské" mobile application, you acknowledge that you have read and understood this Privacy Policy and agree to the collection, use, and disclosure of your information as described herein.</p>
