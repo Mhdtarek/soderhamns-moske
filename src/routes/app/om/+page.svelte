95306580e5a94efa71c5dba0fe8480c9d7a8823b
mhdtarek <mhdtarekalkasemi@gmail.com>
2023-07-08 12:39:03 +0200

    just a lil fix

--- src/routes/app/om/+page.svelte
+++ src/routes/app/om/+page.svelte
@@ -3,9 +3,8 @@
     <h1 style="text-align: center; display: block ">Om</h1>
     <div style="display: grid; place-items: center">
       <a href="https://www.facebook.com/soderhamnsislamiskakulturforening/?locale=sv_SE" target="_blank">
-        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><rect fill="none" height="24" width="24"/><path d="M22,12c0-5.52-4.48-10-10-10S2,6.48,2,12c0,4.84,3.44,8.87,8,9.8V15H8v-3h2V9.5C10,7.57,11.57,6,13.5,6H16v3h-2 c-0.55,0-1,0.45-1,1v2h3v3h-3v6.95C18.05,21.45,22,17.19,22,12z"/></svg>      </a>
+        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="48" viewBox="0 0 24 24" width="48"><rect fill="none" height="24" width="24"/><path d="M22,12c0-5.52-4.48-10-10-10S2,6.48,2,12c0,4.84,3.44,8.87,8,9.8V15H8v-3h2V9.5C10,7.57,11.57,6,13.5,6H16v3h-2 c-0.55,0-1,0.45-1,1v2h3v3h-3v6.95C18.05,21.45,22,17.19,22,12z"/></svg>      </a>
     </div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15323.868006058034!2d17.03137813955078!3d61.308529500000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4666cbe9d9fa2c49%3A0xd2679892a737acf9!2z2KzYp9mF2Lkg2LPZiNiv2LHZh9in2YXZhiAoU8O2ZGVyaGFtbiBtb3Nrw6kgKQ!5e0!3m2!1sen!2sse!4v1688720643398!5m2!1sen!2sse" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </article>
 </main>
 <style>
