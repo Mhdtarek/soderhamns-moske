23dd40ce7c1da3450f5560d09e32be81a2891c55
Tarek <99592820+Mhdtarek@users.noreply.github.com>
2023-07-13 00:00:29 +0200

    Update +page.svelte

--- src/routes/app/donera/+page.svelte
+++ src/routes/app/donera/+page.svelte
@@ -6,7 +6,7 @@
   <h1 style="text-align: center; ">Donera</h1>
   <img src="/swish-QR-large.png" alt="Swish QR kod" height="500px" >
   <div style="display: grid; place-items: center">
-    <a href="https://app.swish.nu/1/p/sw/?sw=1234019146&msg=&src=qr" target="_blank" style="text-align: center;" class="button green10">Tryck här för att donera</a>
+    <a href="https://app.swish.nu/1/p/sw/?sw=1235019146&msg=&src=qr" target="_blank" style="text-align: center;" class="button green10">Tryck här för att donera</a>
   </div>
 </article>
 </main>
