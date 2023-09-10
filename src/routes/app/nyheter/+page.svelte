94d1462da777c7c2897a82939486444c7e32a857
Tarek <99592820+Mhdtarek@users.noreply.github.com>
2023-09-10 11:47:00 +0200

    Fixad datum

--- src/routes/app/nyheter/+page.svelte
+++ src/routes/app/nyheter/+page.svelte
@@ -23,9 +23,6 @@
     year: "numeric",
     month: "long",
     day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
     hour12: false,
     timeZone: "Europe/Stockholm"
   };
--- src/routes/app/nyheter/[slug]/+page.svelte
+++ src/routes/app/nyheter/[slug]/+page.svelte
@@ -7,9 +7,6 @@
     year: "numeric",
     month: "long",
     day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
     hour12: false,
     timeZone: "Europe/Stockholm"
   };
@@ -20,8 +17,8 @@
 <main class=responsive>
   <article>
     <h3>{ data.title }</h3>
-    <p>Lagt ut: {data.date}</p>
-    <p>Skapat av: {formatDate(data.created)}</p>
+    <p>Lagt ut: {formatDate(data.date)}</p>
+    <p>Skapat av: {data.created}</p>
     <a href="/app/nyheter" class="button green5" style="margin-top: 15px; margin-left: 0px">← Gå Tillbaka</a>
   </article>
   <div class="responsive">
