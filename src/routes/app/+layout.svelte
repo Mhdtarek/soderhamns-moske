0b707e6bd0b961453cb9efb0e884deab6c11f617
Tarek <99592820+Mhdtarek@users.noreply.github.com>
2023-09-10 12:50:24 +0200

    Making the experience better

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -9,21 +9,28 @@
   <br>
   <nav class="tertiary-container green3">
     <div class="max" />
-    <a class="transparent square round extra" href="/app">
+    <a class="button square round green8" href="/app">
       <i>home</i>
     </a>
-    <a class="transparent square round extra" href="/app/bonetider">
+    <a class="button square round green8" href="/app/bonetider">
       <i>mosque</i>
     </a>
-    <a class="transparent square round extra" href="/app/nyheter">
+    <a class="button square round green8" href="/app/nyheter">
       <i>newspaper</i>
     </a>
-    <a class="transparent square round extra" href="/app/donera">
-      <i>volunteer_activism</i>
-    </a>
-    <a class="transparent square round extra" href="/app/kontakt">
-      <i>phone</i>
-    </a>
+    <button class="button square round green8">
+      <i>arrow_drop_up</i>
+      <menu class="no-wrap drop-up-menu dark">
+        <a href="/app/donera" class="row">
+          <i>volunteer_activism</i>
+          <span>Donera</span>
+        </a>
+        <a href="/app/kontakt" class="row">
+          <i>phone</i>
+          <span>Kontakta</span>
+        </a>
+      </menu>
+    </button>
     <div class="max" />
   </nav>
 </div>
@@ -43,4 +50,15 @@
     right: 0;
     padding: 0 20px;
   }
+  .drop-up-menu {
+  position: absolute; /* Position the menu absolutely */
+  transform: translateY(-80%) translateX(-25%); /* Move the menu up */
+  z-index: 1; /* Ensure it appears above other elements */
+}
+
+/* Add hover styles for the menu items */
+.drop-up-menu a:hover {
+  background-color: #f0f0f0;
+}
+
 </style>
