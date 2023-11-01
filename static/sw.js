075f51e3c5dbaec878cfb41fad425f873eee90ff
Tarek <99592820+Mhdtarek@users.noreply.github.com>
2023-11-01 20:36:10 +0100

    Temporary commit

--- static/sw.js
+++ static/sw.js
@@ -1,74 +1,51 @@
-// Service Worker Script (sw.js)
+// Define the prayer times
+const prayerTimes = {
+  Fajr: "04:47",
+  Shuruk: "07:08",
+  Dhohr: "11:40",
+  Asr: "13:27",
+  Maghrib: "16:02",
+  Isha: "22:50",
+};
 
-// Set the interval to check for prayer times and display notifications (in milliseconds)
-const notificationInterval = 1000 * 60 * 1; // 15 minutes
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    self.skipWaiting() // Activate the new service worker immediately
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    self.clients.claim() // Take control of all pages under this service worker's scope
-  );
-});
-
-self.addEventListener("push", (event) => {
-  // Handle push events if needed
-});
-
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-
-  // Handle opening the PWA at a specific section (e.g., prayer times page)
-  const fullPath = self.location.origin + "/app/prayerTimes";
-  clients.openWindow(fullPath);
-});
-
-self.addEventListener("notificationclose", (event) => {
-  // Handle notification close event if needed
-});
-
-// Use a background timer to periodically fetch prayer times and schedule notifications
-setInterval(fetchAndSchedulePrayerTimes, notificationInterval);
-
-function fetchAndSchedulePrayerTimes() {
-  fetch("/api/getTodayPrayerTimes")
-    .then((response) => response.json())
-    .then((data) => {
-      // Extract prayer time data
-      const { Fajr, Shuruk, Dhohr, Asr, Maghrib, Isha } = data;
-
-      // Schedule notifications for each prayer time
-      schedulePrayerNotification("Fajr", Fajr);
-      schedulePrayerNotification("Shuruk", Shuruk);
-      schedulePrayerNotification("Dhohr", Dhohr);
-      schedulePrayerNotification("Asr", Asr);
-      schedulePrayerNotification("Maghrib", Maghrib);
-      schedulePrayerNotification("Isha", Isha);
-    })
-    .catch((error) => {
-      console.error("Error fetching prayer times:", error);
-    });
+// Function to display a notification
+function showNotification(title, body) {
+  self.registration.showNotification(title, {
+    body: body,
+  });
 }
 
-function schedulePrayerNotification(prayerName, prayerTime) {
-  const title = `${prayerName} Prayer Time`;
-  const options = {
-    body: `It is time for the ${prayerName} prayer.`,
-    icon: "custom-notification-icon.png",
-    data: {
-      path: "/app/prayerTimes", // Specify the path to open when the notification is clicked
-    },
-  };
+// Function to schedule notifications
+function scheduleNotifications() {
+  const now = new Date();
+  const currentTime =
+    now.getHours() +
+    ":" +
+    (now.getMinutes() < 10 ? "0" : "") +
+    now.getMinutes();
 
-  // Schedule the notification for the given prayer time
-  const timeInMilliseconds = new Date(prayerTime).getTime() - Date.now();
-  if (timeInMilliseconds > 0) {
-    setTimeout(() => {
-      self.registration.showNotification(title, options);
-    }, timeInMilliseconds);
+  for (const prayer in prayerTimes) {
+    if (prayerTimes[prayer] === currentTime) {
+      showNotification(`Time for ${prayer} prayer!`, "It is time to pray.");
+    }
   }
 }
+
+// Set up a timer to check and send notifications
+setInterval(scheduleNotifications, 60000); // Check every minute
+
+// Add an event listener for notification click
+self.addEventListener("notificationclick", function (event) {
+  event.notification.close();
+  // Add your custom action here if needed
+});
+
+// Add an event listener for service worker installation
+self.addEventListener("install", function (event) {
+  // Perform any initial setup here
+});
+
+// Add an event listener for service worker activation
+self.addEventListener("activate", function (event) {
+  // Perform any necessary cleanup here
+});
