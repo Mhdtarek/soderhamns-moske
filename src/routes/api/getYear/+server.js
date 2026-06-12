fd75b40eafb965c1b66c7dc23a1a21eb22ad7274
mhdtarek <mhdtarekalkasemi@gmail.com>
2026-06-12 22:09:38 +0200

    Update year

--- src/routes/api/getYear/+server.js
+++ src/routes/api/getYear/+server.js
@@ -2,5 +2,5 @@
 import { json } from "@sveltejs/kit";
 
 export const GET = async () => {
-  return json({ Year: 2025 }); // Return only the first post
+  return json({ Year: 2026 }); // Return only the first post
 };
