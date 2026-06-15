b57f303f1c6cfd8b6f674c962a99328619d0ecbf
mhdtarek <mhdtarekalkasemi@gmail.com>
2026-06-15 22:51:15 +0200

    New api route

--- .netlify/server/entries/endpoints/api/getYear/_server.js
+++
@@ -1,7 +0,0 @@
-import { j as json } from "../../../../chunks/index.js";
-const GET = async () => {
-  return json({ Year: 2025 });
-};
-export {
-  GET
-};
--- .netlify/server/entries/pages/_page.js
+++
@@ -1,9 +0,0 @@
-import "../../chunks/index.js";
-async function load({ fetch }) {
-  const res = await fetch(`/api/getTodayPrayerTimes`);
-  const data = await res.json();
-  return { prayerTimes: data };
-}
-export {
-  load
-};
